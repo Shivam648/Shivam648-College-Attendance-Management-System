1fee2ebc119586a87a64f42cdef54fa61f7d94ff
Shivam648 <shivamjaiswal648@gmail.com>
2021-12-09 23:30:55 +0530

    Second

--- faculty.php
+++ faculty.php
@@ -1,11 +1,11 @@
-<?php include('db_connect.php');?>
+<?php include('db_connect.php'); ?>
 
 <div class="container-fluid">
-	
+
 	<div class="col-lg-12">
 		<div class="row mb-4 mt-4">
 			<div class="col-md-12">
-				
+
 			</div>
 		</div>
 		<div class="row">
@@ -17,8 +17,8 @@
 					<div class="card-header">
 						<b>List of Faculty</b>
 						<span class="float:right"><a class="btn btn-primary btn-block btn-sm col-sm-2 float-right" href="javascript:void(0)" id="new_faculty">
-					<i class="fa fa-plus"></i> New Faculty
-				</a></span>
+								<i class="fa fa-plus"></i> New Faculty
+							</a></span>
 					</div>
 					<div class="card-body">
 						<table class="table table-condensed table-bordered table-hover">
@@ -34,34 +34,34 @@
 								</tr>
 							</thead>
 							<tbody>
-								<?php 
+								<?php
 								$i = 1;
 								$faculty = $conn->query("SELECT * FROM faculty order by name desc ");
-								while($row=$faculty->fetch_assoc()):
+								while ($row = $faculty->fetch_assoc()) :
 								?>
-								<tr>
-									<td class="text-center"><?php echo $i++ ?></td>
-									<td>
-										<p> <b><?php echo $row['id_no'] ?></b></p>
-									</td>
-									<td>
-										<p> <b><?php echo ucwords($row['name']) ?></b></p>
-									</td>
-									<td class="">
-										 <p> <b><?php echo $row['email'] ?></b></p>
-									</td>
+									<tr>
+										<td class="text-center"><?php echo $i++ ?></td>
+										<td>
+											<p> <b><?php echo $row['id_no'] ?></b></p>
+										</td>
+										<td>
+											<p> <b><?php echo ucwords($row['name']) ?></b></p>
+										</td>
+										<td class="">
+											<p> <b><?php echo $row['email'] ?></b></p>
+										</td>
 
-									<td class="">
-										 <p> <b><?php echo $row['contact'] ?></b></p>
-									</td>
-									<td class="">
-										 <p><b><?php echo  $row['address'] ?></b></p>
-									</td>
-									<td class="text-center">
-										<button class="btn btn-sm btn-outline-primary edit_faculty" type="button" data-id="<?php echo $row['id'] ?>" >Edit</button>
-										<button class="btn btn-sm btn-outline-danger delete_faculty" type="button" data-id="<?php echo $row['id'] ?>">Delete</button>
-									</td>
-								</tr>
+										<td class="">
+											<p> <b><?php echo $row['contact'] ?></b></p>
+										</td>
+										<td class="">
+											<p><b><?php echo  $row['address'] ?></b></p>
+										</td>
+										<td class="text-center">
+											<button class="btn btn-sm btn-outline-primary edit_faculty" type="button" data-id="<?php echo $row['id'] ?>">Edit</button>
+											<button class="btn btn-sm btn-outline-danger delete_faculty" type="button" data-id="<?php echo $row['id'] ?>">Delete</button>
+										</td>
+									</tr>
 								<?php endwhile; ?>
 							</tbody>
 						</table>
@@ -70,56 +70,59 @@
 			</div>
 			<!-- Table Panel -->
 		</div>
-	</div>	
+	</div>
 
 </div>
 <style>
-	
-	td{
+	td {
 		vertical-align: middle !important;
 	}
-	td p{
+
+	td p {
 		margin: unset
 	}
-	img{
-		max-width:100px;
-		max-height: :150px;
+
+	img {
+		max-width: 100px;
+		max-height: 150px;
 	}
 </style>
 <script>
-	$(document).ready(function(){
+	$(document).ready(function() {
 		$('table').dataTable()
 	})
-	
-	$('#new_faculty').click(function(){
-		uni_modal("New faculty","manage_faculty.php","mid-large")
-		
+
+	$('#new_faculty').click(function() {
+		uni_modal("New faculty", "manage_faculty.php", "mid-large")
+
 	})
 
-	$('.view_payment').click(function(){
-		uni_modal("facultys Payments","view_payment.php?id="+$(this).attr('data-id'),"large")
-		
+	$('.view_payment').click(function() {
+		uni_modal("facultys Payments", "view_payment.php?id=" + $(this).attr('data-id'), "large")
+
 	})
-	$('.edit_faculty').click(function(){
-		uni_modal("Manage faculty Details","manage_faculty.php?id="+$(this).attr('data-id'),"mid-large")
-		
+	$('.edit_faculty').click(function() {
+		uni_modal("Manage faculty Details", "manage_faculty.php?id=" + $(this).attr('data-id'), "mid-large")
+
 	})
-	$('.delete_faculty').click(function(){
-		_conf("Are you sure to delete this faculty?","delete_faculty",[$(this).attr('data-id')])
+	$('.delete_faculty').click(function() {
+		_conf("Are you sure to delete this faculty?", "delete_faculty", [$(this).attr('data-id')])
 	})
-	
-	function delete_faculty($id){
+
+	function delete_faculty($id) {
 		start_load()
 		$.ajax({
-			url:'ajax.php?action=delete_faculty',
-			method:'POST',
-			data:{id:$id},
-			success:function(resp){
-				if(resp==1){
-					alert_toast("Data successfully deleted",'success')
-					setTimeout(function(){
+			url: 'ajax.php?action=delete_faculty',
+			method: 'POST',
+			data: {
+				id: $id
+			},
+			success: function(resp) {
+				if (resp == 1) {
+					alert_toast("Data successfully deleted", 'success')
+					setTimeout(function() {
 						location.reload()
-					},1500)
+					}, 1500)
 
 				}
 			}
